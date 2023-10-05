4413203f0ddd4eeb03a6d0c00a26445d8d414fb6
nguyen ba an <thombt@vmogroup.com>
2023-10-05 22:18:52 +0700

    initial project

---
+++ src/main/java/com/vmo/demowebshop/common/GlobalConstants.java
@@ -0,0 +1,7 @@
+package com.vmo.demowebshop.common;
+
+public class GlobalConstants {
+    public static final long SHORT_TIMEOUT = 5;
+    public static final long LONG_TIMEOUT = 60;
+    public static final String DEMOWEBSHOP_URL = "https://demowebshop.tricentis.com/";
+}
